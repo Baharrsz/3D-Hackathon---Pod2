3008d663f22a64debe2b5aba32a795ea398b5c06
Kayle Odegard <mattiasodegard10@gmail.com>
2019-11-22 13:56:40 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
-import Carousel from "./components/Carousel";
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <Carousel />
-      </div>
-    );
+    return <div></div>;
   }
 }
